a83c9da67ea5da8fa1cba81b8b9dd358fe98a0ad
REAN TRY <camboorean@gmail.com>
2025-02-05 23:08:59 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const apps = [
         name: 'Liên Quân Hack Map ✅',
         url: 'https://sub4unlock.io/IJCxp',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/62/cb/24/62cb2406-91ce-f31c-9b6b-6397b88afe24/AppIcon-1x_U007emarketing-0-10-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Chấp Tố - Hack Map - Mod skin'
+        descriptions: 'CapCut Pro'
     },
     {
         name: '𝐊𝐞𝐲 𝐋𝐐 𝐁𝐚̉𝐧 𝐇𝐚𝐜𝐤 𝐌𝐚𝐩',
         url: 'https://sub4unlock.io/8FgUf',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/62/cb/24/62cb2406-91ce-f31c-9b6b-6397b88afe24/AppIcon-1x_U007emarketing-0-10-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'cập nhật ngày 15/01/2025'
+        descriptions: 'Youtube ++'
     },
     {
         name: 'TrollStore X ✅',

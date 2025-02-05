8e8750dab949660a4b3c142b27d8d6627b1bcfe7
REAN TRY <camboorean@gmail.com>
2025-02-05 23:25:29 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const apps = [
         descriptions: 'CapCut Pro'
     },
     {
-        name: '𝐊𝐞𝐲 𝐋𝐐 𝐁𝐚̉𝐧 𝐇𝐚𝐜𝐤 𝐌𝐚𝐩',
+        name: 'YOUTUBE ++✅',
         url: 'https://sub4unlock.io/8FgUf',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/62/cb/24/62cb2406-91ce-f31c-9b6b-6397b88afe24/AppIcon-1x_U007emarketing-0-10-0-85-220-0.png/512x512bb.jpg',
         descriptions: 'Youtube ++'

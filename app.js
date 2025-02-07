9d2f0d7c5524eee1eb1b8699318331693ec7c1ff
REAN TRY <camboorean@gmail.com>
2025-02-08 06:20:00 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const apps = [
     {
         name: 'CAPCUT PRO ✅',
-        url: 'https://reantry2024.github.io/Rean.IOSKH/reantry1.plist',
+        url: 'https://reantry2024.github.io/Rean.IOSKH/plist/reantry1.plist',
         img: 'https://reantry2024.github.io/Rean.IOSKH/img/CapCut-RTY.png',
         descriptions: 'CapCut Pro'
     },
